02d84860aabbbc1bbd2b36d7a5a6e653c9831e11
李宇登 <li@maichong.it>
2016-11-10 18:31:07 +0800

    redux创建，存在bug

---
+++ component/setStore.js
@@ -0,0 +1,15 @@
+/**
+ * @copyright Maichong Software Ltd. 2016 http://maichong.it
+ * @date 2016-11-09
+ * @author Li <li@maichong.it>
+ */
+
+let store;
+function setStore(rStore) {
+  console.log('------>redux,store===', rStore);
+  store = rStore;
+};
+function getStore() {
+  return store;
+}
+export {getStore, setStore};
---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * @copyright Maichong Software Ltd. 2016 http://maichong.it
+ * @date 2016-11-09
+ * @author Li <li@maichong.it>
+ */
+import connect from './component/connect';
+import {getStore, setStore} from './component/setStore';
+export {getStore, setStore, connect};
