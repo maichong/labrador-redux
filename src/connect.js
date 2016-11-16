d7af84b0042b8fd178ad4a5315dc09dad41d2c5a
李宇登 <li@maichong.it>
2016-11-16 12:27:06 +0800

    根据labrador0.5.6更新

--- src/connect.js
+++ src/connect.js
@@ -12,16 +12,16 @@ const defaultMapStateToProps:Function = state =>({});
 export default function connect(mapStateToProps) {
   const shouldSubscribe:Boolean = Boolean(mapStateToProps);
   const mapState:Function = mapStateToProps || defaultMapStateToProps;
-  
+
   return function wrapWithConnect(component) {
     let changeFunc:Function;
-    if(!!component.prototype.onStateChange && typeof component.prototype.onStateChange === 'function'){
-      changeFunc = component.prototype.onStateChange;
-    }else if(!!component.prototype.onStateChange){
+    if (!!component.prototype._onStateChange && typeof component.prototype._onStateChange === 'function') {
+      changeFunc = component.prototype._onStateChange;
+    } else if (!!component.prototype._onStateChange) {
       console.error(`变量名冲突：在"${component.name}.js"中已存在onStateChange方法！`);
     }
-    component.prototype.onStateChange = function () {
-      if (!this.unSubscribe || !this.state) {
+    component.prototype._onStateChange = function () {
+      if (!this.__unSubscribe || !this.state) {
         return;
       }
       let preState:$DataMap = mapState(this.state);
@@ -29,24 +29,33 @@ export default function connect(mapStateToProps) {
       if (shallowEqual(state, preState)) {
         return;
       }
-      this.setState(state);
-      if(!!changeFunc){
+      let nextProps:$DataMap;
+      if (this.props && this.props.merge) {
+        nextProps = this.props.merge(state);
+      } else {
+        nextProps = Object.assign({}, this.props, state);
+      }
+      if (this.onUpdate) {
+        this.onUpdate(nextProps);
+      }
+      this.props = nextProps;
+      if (!!changeFunc) {
         changeFunc(...arguments);
       }
     };
     let func:Function = component.prototype.onLoad;
     component.prototype.onLoad = function () {
       let store:$DataMap = getStore();
-      if(!store){
+      if (!store) {
         console.error('store对象不存在,请前往"app.js"文件中使用"redux"创建store,并传参到"labrador-redux"的setStore()方法中');
       }
       if (shouldSubscribe) {
-        this.unSubscribe = store.subscribe(this.onStateChange.bind(this));
-        this.onStateChange.apply(this);
+        this.__unSubscribe = store.subscribe(this._onStateChange.bind(this));
+        this._onStateChange.apply(this);
       }
-      func.call(this,...arguments);
+      func.call(this, ...arguments);
     };
-    
+
     return component;
   }
 }
